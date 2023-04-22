07015e9c4031c15f5670a6539a45e2b29af8212e
shruttiichodvadiyA <121149502+shruttiichodvadiyA@users.noreply.github.com>
2023-04-22 20:12:08 +0530

    update

--- app/src/main/java/com/example/retrofitproject/MainActivity.kt
+++ app/src/main/java/com/example/retrofitproject/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.example.retrofitproject
 
+import android.R
+import android.app.ProgressDialog
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.ConnectivityManager
 import android.os.Bundle
+import android.util.EventLogTags
 import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.retrofitproject.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import retrofit2.Retrofit
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,9 +35,20 @@ class MainActivity : AppCompatActivity() {
 
     private fun initview() {
 
+//        val conMgr = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+//        val netInfo = conMgr.activeNetworkInfo
+//        if (netInfo == null) {
+//            EventLogTags.Description.setVisibility(View.INVISIBLE)
+//            Retrofit.Builder(this)
+//                .setTitle(resources.getString(R.string.RetroFitProject))
+//                .setMessage(resources.getString(R.string.internet_error))
+//                .setPositiveButton("OK", null).show()
+//        } else {
+//            dialog = ProgressDialog.show(this@WelcomePage, "", "Loading...", true, false)
+//            Welcome_Page().execute()
+//        }
+
         apiInterface = APIClient.getclient().create(APIInterface::class.java)
-
-
         apiInterface.getAllproducts().enqueue(object : retrofit2.Callback<ProductModelClass<Any?>> {
             override fun onResponse(
                 call: Call<ProductModelClass<Any?>>,
@@ -44,26 +63,19 @@ class MainActivity : AppCompatActivity() {
                     Log.e("TAG", "onResponse: " + it.id)
                     startActivity(i)
                 }
-
                 Log.e("TAG", "onResponse: " + productlist?.size)
-
                 var manager = LinearLayoutManager(this@MainActivity)
                 binding.rcvproducts.layoutManager = manager
                 binding.rcvproducts.adapter = adapter
             }
-
             override fun onFailure(call: Call<ProductModelClass<Any?>>, t: Throwable) {
-
             }
-
         })
-
     }
 
     private fun search() {
         binding.btnsearch.setOnClickListener {
             var SearchText=binding.edtsearch.text.toString()
-
             apiInterface.getSearchItem(SearchText).enqueue(object :
                 Callback<ProductModelClass<ProductsItem>> {
                 override fun onResponse(
@@ -76,13 +88,10 @@ class MainActivity : AppCompatActivity() {
                     binding.rcvsearch.layoutManager=manager
                     binding.rcvsearch.adapter=adapter
                 }
-
                 override fun onFailure(call: Call<ProductModelClass<ProductsItem>>, t: Throwable) {
                     TODO("Not yet implemented")
                 }
-
             })
         }
     }
-
 }
